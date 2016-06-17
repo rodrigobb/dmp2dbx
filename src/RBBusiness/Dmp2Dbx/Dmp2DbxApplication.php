7acc762f56125cf450a88e00475a8c61ed1f7e3c
Rodrigo Borrego Bernabé <rodrigobb@gmail.com>
2016-06-18 00:57:14 +0200

    Dump2Dbx turned to an application

---
+++ src/RBBusiness/Dmp2Dbx/Dmp2DbxApplication.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RBBusiness\Dmp2Dbx;
+
+use RBBusiness\Dmp2Dbx\Command\Dmp2DbxCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+
+class Dmp2DbxApplication extends Application
+{
+    /**
+     * Gets the name of the command based on input.
+     *
+     * @param InputInterface $input The input interface
+     *
+     * @return string The command name
+     */
+    protected function getCommandName(InputInterface $input)
+    {
+        // This should return the name of your command.
+        return 'dmp2dbx:upload';
+    }
+
+    /**
+     * Gets the default commands that should always be available.
+     *
+     * @return array An array of default Command instances
+     */
+    protected function getDefaultCommands()
+    {
+        // Keep the core default commands to have the HelpCommand
+        // which is used when using the --help option
+        $defaultCommands = parent::getDefaultCommands();
+
+        $defaultCommands[] = new Dmp2DbxCommand();
+
+        return $defaultCommands;
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument.
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+        // clear out the normal first argument, which is the command name
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
+}
